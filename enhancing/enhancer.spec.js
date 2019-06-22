9c7b15a65b2f0f682ef9c996b94a9db0b953e687
Mychal <hallmychal@gmail.com>
2019-06-22 09:35:18 -0500

    repair tested

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+const gladius = {
+  name: "gladius",
+  durability: 50,
+  enhancement: 2
+};
+
+describe("repair", () => {
+  it("creates a new object", () => {
+    expect(enhancer.repair(gladius)).not.toBe(gladius);
+  });
+  it("sets new item durability to 100", () => {
+    const newItem = gladius;
+    expect(enhancer.repair(gladius).durability).toBe(100);
+  });
+});
